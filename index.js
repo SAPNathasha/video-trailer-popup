df9f8b1cef8f1e0bc20c2cbf2c43e11b7588a163
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-19 22:34:13 +0530

    Comment and finish the project

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+//Ensure DOM is fully loaded 
 $(document).ready(function () {
+    //Select elements using jQuery
     const $btnEl = $(".btn");
     const $closeIconEl = $(".close-icon");
     const $trailerContainerEl = $(".trailer-container");
     const $videoEl = $("video");
   
+    // Event listner for button click
     $btnEl.on("click", function () {
       $trailerContainerEl.removeClass("active");
     });
   
+
+    // Event listner for the close icon click
     $closeIconEl.on("click", function () {
       $trailerContainerEl.addClass("active");
       $videoEl.get(0).pause();
